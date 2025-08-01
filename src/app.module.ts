8c2c418021cafbb90e058262fba45b073a7f1063
Andika Pradnyana <andikapradnyana16@gmail.com>
2025-08-01 18:28:27 +0800

    feat: start developing category module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { CommonModule } from './common/common.module';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
+import { CategoryModule } from './category/category.module';
+import { TagModule } from './tag/tag.module';
+import { CommentModule } from './comment/comment.module';
 
 @Module({
-  imports: [CommonModule, UserModule, PostModule],
+  imports: [CommonModule, UserModule, PostModule, CategoryModule, TagModule, CommentModule],
   controllers: [],
   providers: [],
 
